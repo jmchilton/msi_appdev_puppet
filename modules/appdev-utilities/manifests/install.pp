e3c024046c92a53d53de3307f3dbd8a4610fa432
John Chilton <jmchilton@gmail.com>
2012-02-17 08:54:29 -0600

    Fix typo

--- modules/appdev-utilities/manifests/install.pp
+++ modules/appdev-utilities/manifests/install.pp
@@ -2,7 +2,7 @@ class appdev-utilities::install {
   $appdev_utility_packages = ["emacs23-nox", "wajig"]
 
   package { $appdev_utility_packages:
-    require: latest
+    require => latest,
   }
 
 }
