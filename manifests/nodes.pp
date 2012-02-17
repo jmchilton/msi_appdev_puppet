0d639066c04bacc9d73772376f557fad1670edbf
John Chilton <jmchilton@gmail.com>
2012-02-17 09:18:00 -0600

    Update nodes.pp to reflect staging is on spider.msi instead of appdev-dom0.msi.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,21 +2,21 @@ import "basic.pp"
 import "sitedefs.pp"
 import "credentials.pp"
 
-node 'appdev-dom0.msi.umn.edu' {
+node 'spider.msi.umn.edu' {
      $nova_admin_user = "admin"
-     $api_vip = "appdev-dom0.msi.umn.edu"
+     $api_vip = "spider.msi.umn.edu"
      $mysql_vip = "localhost"
-     $rabbit_vip = "appdev-dom0.msi.umn.edu"
-     $keystone_vip = "appdev-dom0.msi.umn.edu"
-     $glance_vip = "appdev-dom0.msi.umn.edu"
+     $rabbit_vip = "spider.msi.umn.edu"
+     $keystone_vip = "spider.msi.umn.edu"
+     $glance_vip = "spider.msi.umn.edu"
      $network_manager = "FlatDHCPManager"
      $fixed_range = "10.1.0.0/16"
      $flat_interface = "br100"
      $dhcp_start= "10.1.0.2"
-     $newtork_host = "appdev-dom0"
+     $newtork_host = "spider"
      $public_interface = "eth0"
      package { memcached: }
 
      include "nova-infra-node"
-     include "nova-compute-node"
+     #include "nova-compute-node"
 }
