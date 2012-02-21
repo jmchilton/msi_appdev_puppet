1884866b5595b41fd53f69fe27c9187c05067e2e
John Chilton <jmchilton@gmail.com>
2012-02-21 13:02:01 -0600

    Improvements to networking, externalized host, interface, and dhcp definitions to a globally defined hash.

--- manifests/cloud_staging.pp
+++ manifests/cloud_staging.pp
@@ -1,6 +1,19 @@
 import "sitedefs.pp"
 import "credentials.pp"
 
+     $network = {
+       'node-management' => { 'hostname' => 'spider', 'public_ip' => '128.101.191.208', 'management_ip' => '172.0.0.1' },
+       'node-services' => { 'hostname' => 'spider-services', 'public_ip' => '128.101.191.209', 'management_ip' => '172.0.0.2'  },
+       'node00' => { 'hostname' => 'spider00', 'public_ip' => '128.101.191.210', 'management_ip' => '172.0.0.100', 'mac' => '00:30:48:C4:79:40' },
+       'node01' => { 'hostname' => 'spider01', 'public_ip' => '128.101.191.211', 'management_ip' => '172.0.0.101', 'mac' => '00:30:48:7F:C4:1A' },
+       'node02' => { 'hostname' => 'spider02', 'public_ip' => '128.101.191.212', 'management_ip' => '172.0.0.102', 'mac' => '00:30:48:7F:C3:4C' },
+       'node03' => { 'hostname' => 'spider03', 'public_ip' => '128.101.191.213', 'management_ip' => '172.0.0.103', 'mac' => '00:30:48:7F:C4:32' },
+       'node04' => { 'hostname' => 'spider04', 'public_ip' => '128.101.191.214', 'management_ip' => '172.0.0.104', 'mac' => '00:30:48:7F:E0:BC' },
+       'node05' => { 'hostname' => 'spider05', 'public_ip' => '128.101.191.215', 'management_ip' => '172.0.0.105', 'mac' => '00:30:48:7F:E4:28' },
+       'node06' => { 'hostname' => 'spider06', 'public_ip' => '128.101.191.216', 'management_ip' => '172.0.0.106', 'mac' => '00:30:48:7F:45:B2' },
+       'node07' => { 'hostname' => 'spider07', 'public_ip' => '128.101.191.217', 'management_ip' => '172.0.0.107', 'mac' => '00:30:48:7F:2C:1C' },
+     }
+
 node 'spider.msi.umn.edu' {
      $nova_admin_user = "admin"
      $api_vip = "spider.msi.umn.edu"
@@ -13,32 +26,28 @@ node 'spider.msi.umn.edu' {
      $flat_interface = "br100"
      $dhcp_start= "10.1.0.2"
      $newtork_host = "spider"
-     $public_interface = "eth0"
+     $public_interface = "eth1"
 
-     $network = { 
-       '-managemnt' => { 'public_ip' => '128.101.191.208', 'management_ip' => '172.0.0.1', },
-       '-services' => { 'public_ip' => '128.101.191.209', 'management_ip' => '172.0.0.2',  },
-       '00' => { 'public_ip' => '128.101.191.210', 'management_ip' => '172.0.0.100', 'mac' => '00:30:48:C4:79:40' },
-       '01' => { 'public_ip' => '128.101.191.211', 'management_ip' => '172.0.0.101', 'mac' => '00:30:48:7F:C4:1A' },
-       '02' => { 'public_ip' => '128.101.191.212', 'management_ip' => '172.0.0.102', 'mac' => '00:30:48:7F:C3:4C' },
-       '03' => { 'public_ip' => '128.101.191.213', 'management_ip' => '172.0.0.103', 'mac' => '00:30:48:7F:C4:32' },
-       '04' => { 'public_ip' => '128.101.191.214', 'management_ip' => '172.0.0.104', 'mac' => '00:30:48:7F:E0:BC' },
-       '05' => { 'public_ip' => '128.101.191.215', 'management_ip' => '172.0.0.105', 'mac' => '00:30:48:7F:E4:28' },
-       '06' => { 'public_ip' => '128.101.191.216', 'management_ip' => '172.0.0.106', 'mac' => '00:30:48:7F:45:B2' },
-       '07' => { 'public_ip' => '128.101.191.217', 'management_ip' => '172.0.0.107', 'mac' => '00:30:48:7F:2C:1C' },
-    }
+     $network_node = 'node-management'
 
-    #include 'management-network'
-    management-network( "node-management" : node => "node-management" )
-
-     #package { memcached: }
-
+     include 'management-network'
      include 'appdev-base'
      include 'bootstrapper'
+
      #include "nova-infra-node"
      #include "nova-compute-node"
 }
 
 node 'spider-services.msi.umn.edu' {
+     $network_node = 'node-services'  
+
      include 'appdev-base'
+     include 'management-network'
 }
+
+node /^spider\d+.msi.umn.edu/ {
+     $network_node = regsubst($hostname, 'spider(\d+)', 'node\1')
+     include 'appdev-base'
+     include 'management-network'
+}
+
--- modules/management-network/manifests/init.pp
+++ modules/management-network/manifests/init.pp
@@ -1,10 +1,10 @@
+class management-network {
 
-def management-network ( $node ) {
-  file { "/etc/hosts-fake": 
+  file { "/etc/hosts-fake":
     content => template("management-network/hosts.erb"),
   }
 
-  file { "/etc/networking/interfaces-fake":
+  file { "/etc/network/interfaces-fake":
     content => template("management-network/interfaces.erb"),
   }
 
